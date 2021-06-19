588e1ac531eb857073798157c4dfedef82b3ed54
Una Ada <trewbot@gmail.com>
2021-06-18 19:25:31 -0700

    feat(passport): add callback to OAuth strategy

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 /**
  * @file Passport configuration.
  * @author Una Ada <una@anarchy.website>
- * @version 0.1.5
+ * @version 0.1.6
  * @since 0.1.5
  */
 
@@ -9,6 +9,7 @@
 import dotenv from 'dotenv';
 import passport from 'passport';
 import { OAuth2Strategy as GoogleStrategy } from 'passport-google-oauth';
+import User from '../models/user.js';
 
 /*----- Initialize -----------------------------------------------------------*/
 dotenv.config();
@@ -19,6 +20,26 @@ passport.use(
       clientSecret: process.env.GOOGLE_SECRET,
       callbackURL: process.env.GOOGLE_CALLBACK,
     },
-    (accessToken, refreshToken, profile, next) => {}
+    (accessToken, refreshToken, profile, next) => {
+      User.findOne({ googleId: profile.id }, (err, user) => {
+        err
+          ? console.error(err) || next(err)
+          : user
+          ? next(null, user)
+          : User.create(
+              {
+                email: profile.emails[0].value,
+                // Temporary method to create a handle
+                handle: profile.emails[0].value.split('@')[0],
+                displayName: profile.displayName,
+                googleId: profile.id,
+              },
+              (err, newUser) =>
+                err ? console.error(err) || next(err) : next(null, newUser)
+            );
+      });
+    }
   )
 );
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser((id, done) => User.findById(id, done));
