58680f62aa128df54276ec54b9ad5a9a9b9bc7e8
Una Ada <trewbot@gmail.com>
2021-06-19 21:47:24 -0700

    feat(route): add user show route

--- routes/users.js
+++ routes/users.js
@@ -14,6 +14,7 @@ import usersCtrl from '../controllers/users.js';
 
 /*----- Routes ---------------------------------------------------------------*/
 const router = Router();
+router.get('/:handle', (req, res) => res.send(req.params.handle));
 router
   .route('/settings')
   .all((req, res, next) =>
