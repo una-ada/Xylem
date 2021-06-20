bac652ca0e0e88ab6389981fe7490a19f94ba01f
Una Ada <trewbot@gmail.com>
2021-06-19 21:37:26 -0700

    feat(model): add Post model

---
+++ models/post.js
@@ -0,0 +1,27 @@
+/**
+ * Post model definition.
+ * @author Una Ada <una@anarchy.website>
+ * @version 0.3.0
+ * @since 0.3.0
+ * @module models/post
+ */
+
+/*----- Imports --------------------------------------------------------------*/
+import mongoose from 'mongoose';
+
+/*------- Schema -------------------------------------------------------------*/
+const postSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    content: {
+      type: String
+    }
+  },
+  { timestamps: true }
+  );
+
+/*----- Exports --------------------------------------------------------------*/
+export default mongoose.model('Post', postSchema);
