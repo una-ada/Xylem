b4ebc4d1dc4703fef49df7c39edd690bb9e8b851
Una Ada <trewbot@gmail.com>
2021-06-23 12:09:03 -0700

    feat(model): add like schema

--- models/post.js
+++ models/post.js
@@ -1,7 +1,7 @@
 /**
  * Post model definition.
  * @author Una Ada <una@anarchy.website>
- * @version 0.3.1
+ * @version 0.5.0
  * @since 0.3.0
  * @module models/post
  * @see module:controllers/post
@@ -12,18 +12,23 @@
 import mongoose from 'mongoose';
 
 /*------- Schema -------------------------------------------------------------*/
-const postSchema = new mongoose.Schema(
-  {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
+const likeSchema = new mongoose.Schema(
+    { user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } },
+    { timestamps: true }
+  ),
+  postSchema = new mongoose.Schema(
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+      title: String,
+      content: String,
+      contentRich: String,
+      likes: [likeSchema],
     },
-    title: String,
-    content: String,
-    contentRich: String,
-  },
-  { timestamps: true }
-);
+    { timestamps: true }
+  );
 
 /*----- Exports --------------------------------------------------------------*/
 export default mongoose.model('Post', postSchema);
