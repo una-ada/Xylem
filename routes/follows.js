73d0e18ae545d9dfa07584b473937cb8fbfc3cc8
Una Ada <trewbot@gmail.com>
2021-06-24 08:21:10 -0700

    feat(route): add follows router

---
+++ routes/follows.js
@@ -0,0 +1,21 @@
+/**
+ * Follows router to be mounted on /users
+ * @author Una Ada <una@anarchy.website>
+ * @version 0.6.0
+ * @since 0.6.0
+ * @module controllers/follows
+ * @see module:models/follow
+ */
+
+/*------ Imports -------------------------------------------------------------*/
+import { Router } from 'express';
+
+/*----- Methods --------------------------------------------------------------*/
+const checkUser = (req, res, next) =>
+  req.user !== undefined ? next() : res.sendStatus(403);
+
+/*------ Routes --------------------------------------------------------------*/
+const router = new Router();
+
+/*------ Exports -------------------------------------------------------------*/
+export default router;
