ea7222b5f7f6b5bbdc2acbd00770ee71ce8b9884
Una Ada <trewbot@gmail.com>
2021-06-23 20:46:46 -0700

    feat(model): add comment schema

--- models/post.js
+++ models/post.js
@@ -1,7 +1,7 @@
 /**
  * Post model definition.
  * @author Una Ada <una@anarchy.website>
- * @version 0.5.0
+ * @version 0.5.3
  * @since 0.3.0
  * @module models/post
  * @see module:controllers/post
@@ -16,6 +16,17 @@ const likeSchema = new mongoose.Schema(
     { user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } },
     { timestamps: true }
   ),
+  commentSchema = new mongoose.Schema(
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+      content: String,
+      contentRich: String,
+    },
+    { timestamps: true }
+  ),
   postSchema = new mongoose.Schema(
     {
       user: {
@@ -26,6 +37,7 @@ const likeSchema = new mongoose.Schema(
       content: String,
       contentRich: String,
       likes: [likeSchema],
+      comments: [commentSchema],
     },
     { timestamps: true }
   );
